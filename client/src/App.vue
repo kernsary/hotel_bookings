c2ee1c624f949c4a7e14f31a110e84cb2b6d307b
Charles Kennedy <charlesscottkennedy@googlemail.com>
2019-10-30 13:13:23 +0000

    delete buttons added

--- client/src/App.vue
+++ client/src/App.vue
@@ -24,6 +24,13 @@ export default {
 
   mounted() {
     this.fetchData();
+
+    eventBus.$on('booking-added', booking => this.bookings.push(booking));
+
+    eventBus.$on('booking-deleted', id => {
+      const index = this.bookings.findIndex(booking => booking._id === id);
+      this.bookings.splice(index, 1);
+    })
   },
 
   methods: {
--- client/src/components/BookingsList.vue
+++ client/src/components/BookingsList.vue
@@ -1,9 +1,10 @@
 <template lang="">
   <div id="bookingsList">
-  <div class="bookings" v-for="booking in bookings">
-    <p>{{booking.name}}, {{booking.email}}, Checked in? {{booking.checkedIn}}</p>
+    <div class="bookings" v-for="booking in bookings">
+      <p>{{booking.name}}, {{booking.email}}, Checked in? {{booking.checkedIn}}</p>
+      <button @click="handleDelete(booking._id)">Delete</button>
+    </div>
   </div>
-</div>
 </template>
 
 <script>
@@ -12,9 +13,22 @@ import BookingService from '../services/BookingService'
 
 export default {
   name: 'bookings-list',
-  props: ['bookings']
+  props: ['bookings'],
+
+  methods: {
+    handleDelete(id){
+      BookingService.deleteBooking(id)
+      .then(response => eventBus.$emit('booking-deleted', id));
+    }
+  }
 }
 </script>
 
 <style lang="css" scoped>
+
+#bookingsList {
+  font-family: Helvetica, sans-serif;
+  font-size: 0.5em;
+}
+
 </style>
