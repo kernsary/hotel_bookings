b850ac7e26445dc94b9b457082d4c504f66dbd11
Charles Kennedy <charlesscottkennedy@googlemail.com>
2019-10-30 11:53:27 +0000

    routes and fetch added

---
+++ client/src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <div id="app">
+
+  </div>
+</template>
+
+<script>
+import BookingForm from './components/BookingForm';
+import BookingsList from './components/BookingsList';
+import { eventBus } from './main';
+import BookingService from './services/BookingService'
+
+export default {
+  name: 'app',
+  data() {
+    return {
+      bookings: []
+    }
+  },
+  components: {
+    'booking-form': BookingForm,
+    'bookings-list': BookingsList
+  },
+
+  mounted() {
+    this.fetchData();
+  },
+
+  methods: {
+    fetchData(){
+      BookingService.getBookings()
+      .then(bookings => this.bookings = bookings);
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
